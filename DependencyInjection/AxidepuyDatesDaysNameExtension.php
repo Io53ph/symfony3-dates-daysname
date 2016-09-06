77c4c52e7a3389c6c6678b7ec487d54af40de5af
Jozsef Balogh <jozsef.balogh.jb@gmail.com>
2016-09-06 17:06:02 +0200

    Adding content to the file

---
+++ DependencyInjection/AxidepuyDatesDaysNameExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Axidepuy\Bundle\AxidepuyDatesDaysNameBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * Loads and manages the bundle configuration
+ */
+class AxidepuyDatesDaysNameExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
